d9bfceb1a7b3983db46abe7585248bb558e2cfb2
ivelthoven-linx <51231539+ivelthoven-linx@users.noreply.github.com>
2019-07-01 12:23:01 +0200

    Router added, reqShopify added

--- src/index.js
+++ src/index.js
@@ -1,49 +1,48 @@
-require("isomorphic-fetch");
+// require("isomorphic-fetch");
 const { default: shopifyAuth } = require("@shopify/koa-shopify-auth");
 const { verifyRequest } = require("@shopify/koa-shopify-auth");
 const session = require("koa-session");
 
-const router = require("./router");
+const routers = require("./router");
 
 const Koa = require("koa");
 
 const app = new Koa();
 
 const { SHOPIFY_API_KEY, SHOPIFY_API_SECRET_KEY } = process.env;
+const PORT = process.env.PORT | 3000;
 
-app.use(router.routes()).use(router.allowedMethods());
+routers.forEach(router => {
+  app.use(router.routes()).use(router.allowedMethods());
+});
 
-app
-  .use(session(app))
-  .use(
-    shopifyAuth({
-      prefix: "/shopify",
-      apiKey: SHOPIFY_API_KEY,
-      secret: SHOPIFY_API_SECRET_KEY,
-      scopes: ["write_orders, write_products"],
-      accessMode: "offline",
-      afterAuth(ctx) {
-        const { shop, accessToken } = ctx.session;
+app.use(session(app)).use(
+  shopifyAuth({
+    apiKey: SHOPIFY_API_KEY,
+    secret: SHOPIFY_API_SECRET_KEY,
+    scopes: ["write_orders, write_products"],
+    accessMode: "offline",
+    afterAuth(ctx) {
+      const { shop, accessToken } = ctx.session;
 
-        console.log(shop);
-        console.log("We did it!", accessToken);
+      // Save everything to db
+      console.log(shop);
+      console.log("We did it!", accessToken);
 
-        ctx.redirect("/");
-      }
-    })
-  )
-  .use(
-    verifyRequest({
-      // path to redirect to if verification fails
-      // defaults to '/auth'
-      authRoute: "/shopify/auth",
-      // path to redirect to if verification fails and there is no shop on the query
-      // defaults to '/auth'
-      fallbackRoute: "/shopify/auth"
-    })
-  )
-  .use(ctx => {
-    ctx.body = "🎉";
-  });
+      ctx.redirect("/");
+    }
+  })
+);
+// .use(
+//   verifyRequest({
+//     // authRoute: "/shopify/auth",
+//     // fallbackRoute: "/shopify/auth"
+//   })
+// );
+// .use(ctx => {
+//   ctx.body = "🎉";
+// });
 
-app.listen(3000);
+app.listen(PORT, () => {
+  console.log(`> listening on port ${PORT}`);
+});
